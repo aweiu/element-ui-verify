b560b78ebc863225e157c3473bc261032fac9dd6
awei.yu <awei.yu@2zhuzi.com>
2017-12-28 20:48:23 +0800

    规范ruleName冲突的异常提示

--- src/rules.ts
+++ src/rules.ts
@@ -5,7 +5,7 @@ function rules (): RuleGetters
 function rules (name: string, getter?: RuleGetter): RuleGetter
 function rules (name?: string, getter?: RuleGetter): any {
   if (!name) return getters
-  if (getters[name] && getter) throw Error(`the name ${name} has been used`)
+  if (getters[name] && getter) throw Error(`the rule name '${name}' has been used`)
   if (getter) getters[name] = getter
   return getters[name]
 }
