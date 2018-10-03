3bca8312eeb1920241bac4dcf3952e0a892d18d3
aweiu <328080339@qq.com>
2018-10-03 14:12:29 +0800

    fix #23

--- src/component.ts
+++ src/component.ts
@@ -44,40 +44,46 @@ export default class ElFormItemVerifyComponent extends Vue {
     return this.verify !== undefined && (this as any).prop
   }
 
-  getRules (): object[] {
+  getRules (): any[] {
     if (!this._verify) return ElFormItemMethods.getRules.apply(this, arguments)
+    let asyncVerifyRules: any[] = []
     // 空检测
     let fieldValue = (this as any).fieldValue
     fieldValue = [undefined, null].includes(fieldValue) ? '' : fieldValue + ''
     if (this.space === undefined) fieldValue = fieldValue.trim()
     if (fieldValue === '') {
-      return [{
+      asyncVerifyRules.push({
         validator: (rule: any, val: any, callback: Function) => {
           if (this.canBeEmpty !== undefined || (this as any).minLength <= 0) callback()
           else callback(Error(this.emptyMessage || errorMessage.get('empty')))
         }
-      }]
-    }
-    // 合并普通规则
-    let asyncVerifyRules: object[] = []
-    const ruleGetters = rules()
-    for (let name in ruleGetters) {
-      const ruleVal = (this as any)[name]
-      if (ruleVal !== undefined) asyncVerifyRules = asyncVerifyRules.concat(ruleGetters[name](ruleVal))
-    }
-    // 统一处理错误提示（代码块放在此处可以只针对普通规则）
-    if (this.errorMessage !== undefined) {
-      for (let rule of asyncVerifyRules) (rule as any).message = this.errorMessage
-    }
-    // 自定义校验方法置后
-    if (typeof this.verify === 'function') asyncVerifyRules.push({ validator: this.verify })
-    // 当规则为空时，返回一个始终通过的规则来避免空检测错误无法清除
-    // 也可以通过(this as any).clearValidate()的方式实现，不过不太好
-    return asyncVerifyRules.length === 0 ? [{
-      validator (rule: any, val: any, callback: Function) {
-        callback()
+      })
+    } else {
+      // 合并普通规则
+      const ruleGetters = rules()
+      for (let name in ruleGetters) {
+        const ruleVal = (this as any)[name]
+        if (ruleVal !== undefined) asyncVerifyRules = asyncVerifyRules.concat(ruleGetters[name](ruleVal))
       }
-    }] : asyncVerifyRules
+      // 统一处理错误提示（代码块放在此处可以只针对普通规则）
+      if (this.errorMessage !== undefined) {
+        for (let rule of asyncVerifyRules) (rule as any).message = this.errorMessage
+      }
+      // 自定义校验方法置后
+      if (typeof this.verify === 'function') asyncVerifyRules.push({ validator: this.verify })
+      // 当规则为空时，返回一个始终通过的规则来避免空检测错误无法清除
+      // 也可以通过(this as any).clearValidate()的方式实现，不过不太好
+      if (asyncVerifyRules.length === 0) {
+        asyncVerifyRules.push({
+          validator (rule: any, val: any, callback: Function) {
+            callback()
+          }
+        })
+      }
+    }
+    // 使elementUI可以检测到必填项从而展示*号
+    asyncVerifyRules[0].required = this.canBeEmpty === undefined
+    return asyncVerifyRules
   }
 
   // 兼容<2.0.0-beta.1
