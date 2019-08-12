88914bcc30a7fe4cd11ad212264d4e08bb434b28
aweiu <328080339@qq.com>
2019-08-12 14:16:45 +0800

    数字相关的校验规则不再要求强数字类型

--- src/index.ts
+++ src/index.ts
@@ -10,134 +10,155 @@ export interface VerifyRulePropOptions extends PropOptions {
   name: string;
 }
 
-let installed = false;
 let ElFormItemComponent: VueConstructor;
-const exp = {
-  install(
-    Vue: VueConstructor<any>,
-    options: {
-      errorMessageTemplate?: ErrorMessageTemplate;
-      fieldChange?: "clear" | "verify";
-    } = {}
-  ) {
-    if (installed) return;
-    installed = true;
-    ElFormItemComponent = Vue.component("ElFormItem");
-    if (!ElFormItemComponent) throw Error("please install element-ui first");
-    errorMessage.setTemplate(
-      options.errorMessageTemplate || defaultErrorMessageTemplate
-    );
-    Component.fieldChange = options.fieldChange || "verify";
-    ElFormItemComponent.mixin(Component);
-    init();
-  },
-  addRule(
-    name: string | VerifyRulePropOptions,
-    getter: RuleGetter
-  ): RuleGetter {
-    if (!installed) throw Error("please install me");
-    const props: any = {};
-    if (typeof name === "string") props[name] = {};
-    else props[name.name] = name;
-    const _name = typeof name === "string" ? name : name.name;
-    const component: any = { props };
-    // 监听prop变化，触发校验
-    component.watch = {};
-    component.watch[_name] = function() {
-      if (this.verify !== undefined && (this as any).prop)
-        (this as any).validate("");
-    };
-    ElFormItemComponent.mixin(component);
-    return rules(_name, getter);
-  },
-  getRule(name: string): RuleGetter {
-    return rules(name);
-  },
-  getErrorMessage(name: string, templateData?: any): string {
-    return errorMessage.get(name, templateData);
-  }
-};
+
+function addRule(
+  name: string | VerifyRulePropOptions,
+  getter: RuleGetter
+): RuleGetter {
+  if (!ElFormItemComponent) throw Error("please install me");
+  const props: any = {};
+  if (typeof name === "string") props[name] = {};
+  else props[name.name] = name;
+  const _name = typeof name === "string" ? name : name.name;
+  const component: any = { props };
+  // 监听prop变化，触发校验
+  component.watch = {};
+  component.watch[_name] = function() {
+    if (this.verify !== undefined && (this as any).prop)
+      (this as any).validate("");
+  };
+  ElFormItemComponent.mixin(component);
+  return rules(_name, getter);
+}
+
+function getRule(name: string): RuleGetter {
+  return rules(name);
+}
+
+function getErrorMessage(name: string, templateData?: any): string {
+  return errorMessage.get(name, templateData);
+}
 
 function init() {
+  const transform = (value: string) => Number(value);
   // 文本长度
-  exp.addRule({ name: "length", type: Number }, length => ({
+  addRule({ name: "length", type: Number }, length => ({
     len: length,
-    message: exp.getErrorMessage("length", length)
+    message: getErrorMessage("length", length)
   }));
   // 最小文本长度
-  exp.addRule({ name: "minLength", type: Number }, minLength => ({
+  addRule({ name: "minLength", type: Number }, minLength => ({
     min: minLength,
-    message: exp.getErrorMessage("minLength", minLength)
+    message: getErrorMessage("minLength", minLength)
   }));
-  // 数字类型
-  exp.addRule("number", () => ({
-    type: "number",
-    message: exp.getErrorMessage("number")
+  // 数字
+  addRule("number", () => ({
+    validator(rule: any, val: string, callback: Function) {
+      if (/^([-+])?\d+(\.\d+)?$/.test(val)) callback();
+      else callback(Error(getErrorMessage("number")));
+    }
   }));
   // gt
-  exp.addRule({ name: "gt", type: Number }, gt => [
-    exp.getRule("number")(),
+  addRule({ name: "gt", type: Number }, gt => [
+    getRule("number")(),
     {
       validator(rule: any, val: number, callback: Function) {
-        if (val <= gt) callback(Error(exp.getErrorMessage("gt", gt)));
-        else callback();
+        if (val > gt) callback();
+        else callback(Error(getErrorMessage("gt", gt)));
       }
     }
   ]);
   // gte
-  exp.addRule({ name: "gte", type: Number }, gte => [
-    exp.getRule("number")(),
-    { type: "number", min: gte, message: exp.getErrorMessage("gte", gte) }
+  addRule({ name: "gte", type: Number }, gte => [
+    getRule("number")(),
+    {
+      type: "number",
+      min: gte,
+      transform,
+      message: getErrorMessage("gte", gte)
+    }
   ]);
   // lt
-  exp.addRule({ name: "lt", type: Number }, lt => [
-    exp.getRule("number")(),
+  addRule({ name: "lt", type: Number }, lt => [
+    getRule("number")(),
     {
       validator(rule: any, val: number, callback: Function) {
-        if (val >= lt) callback(Error(exp.getErrorMessage("lt", lt)));
-        else callback();
+        if (val < lt) callback();
+        else callback(Error(getErrorMessage("lt", lt)));
       }
     }
   ]);
   // lte
-  exp.addRule({ name: "lte", type: Number }, lte => [
-    exp.getRule("number")(),
-    { type: "number", max: lte, message: exp.getErrorMessage("lte", lte) }
+  addRule({ name: "lte", type: Number }, lte => [
+    getRule("number")(),
+    {
+      type: "number",
+      max: lte,
+      transform,
+      message: getErrorMessage("lte", lte)
+    }
   ]);
   // 整数类型
-  exp.addRule("int", () => ({
-    type: "integer",
-    message: exp.getErrorMessage("int")
-  }));
-  // 最多小数位
-  exp.addRule({ name: "maxDecimalLength", type: Number }, maxDecimalLength => [
-    exp.getRule("number")(),
+  addRule("int", () => [
+    getRule("number")(),
     {
-      validator(rule: any, val: number, callback: Function) {
+      type: "integer",
+      transform,
+      message: getErrorMessage("int")
+    }
+  ]);
+  // 最多小数位
+  addRule({ name: "maxDecimalLength", type: Number }, maxDecimalLength => [
+    getRule("number")(),
+    {
+      validator(rule: any, val: string | number, callback: Function) {
         const decimal = val.toString().split(".")[1];
         if (decimal && decimal.length > maxDecimalLength)
           callback(
-            Error(exp.getErrorMessage("maxDecimalLength", maxDecimalLength))
+            Error(getErrorMessage("maxDecimalLength", maxDecimalLength))
           );
         else callback();
       }
     }
   ]);
   // 手机号 https://github.com/aweiu/element-ui-verify/issues/24
-  exp.addRule("phone", () => ({
+  addRule("phone", () => ({
     pattern: /^(?=\d{11}$)^1(?:3\d|4[57]|5[^4\D]|6[67]|7[^249\D]|8\d|9[189])\d{8}$/,
-    message: exp.getErrorMessage("phone")
+    message: getErrorMessage("phone")
   }));
   // 邮箱
-  exp.addRule("email", () => ({
+  addRule("email", () => ({
     type: "email",
-    message: exp.getErrorMessage("email")
+    message: getErrorMessage("email")
   }));
   // 6位数字验证码
-  exp.addRule("verifyCode", () => ({
+  addRule("verifyCode", () => ({
     pattern: /^\d{6}$/,
-    message: exp.getErrorMessage("verifyCode")
+    message: getErrorMessage("verifyCode")
   }));
 }
 
-export default exp;
+function install(
+  Vue: VueConstructor<any>,
+  options: {
+    errorMessageTemplate?: ErrorMessageTemplate;
+    fieldChange?: "clear" | "verify";
+  } = {}
+) {
+  ElFormItemComponent = Vue.component("ElFormItem");
+  if (!ElFormItemComponent) throw Error("please install element-ui first");
+  errorMessage.setTemplate(
+    options.errorMessageTemplate || defaultErrorMessageTemplate
+  );
+  Component.fieldChange = options.fieldChange || "verify";
+  ElFormItemComponent.mixin(Component);
+  init();
+}
+
+export default {
+  install,
+  addRule,
+  getRule,
+  getErrorMessage
+};
