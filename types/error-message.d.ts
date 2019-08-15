ca107347ce477f79934416b39045abce72e80377
aweiu <328080339@qq.com>
2019-08-15 12:22:16 +0800

    修复 typescript 环境下的编译错误

--- types/error-message.d.ts
+++
@@ -1,7 +0,0 @@
-import { ErrorMessageTemplate } from "./types";
-declare const _default: {
-    setTemplate(errorMessageTemplate: ErrorMessageTemplate): void;
-    macroToValue(msg: string, macro: string, value: any): string;
-    get(name: string, templateData?: any): string;
-};
-export default _default;
--- types/rules.d.ts
+++
@@ -1,7 +0,0 @@
-import { RuleGetter } from "./types";
-export declare type RuleGetters = {
-    [name: string]: RuleGetter;
-};
-declare function rules(): RuleGetters;
-declare function rules(name: string, getter?: RuleGetter): RuleGetter;
-export default rules;
